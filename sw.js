1ce0169cec5b091bc6b9ae922bdb4c3132126d48
shadyhegab2-svg <shadyhegab2@gmail.com>
2026-06-16 07:11:00 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE='asad-v8';
+const ASSETS=['./', './index.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(res=>{if(res&&res.status===200&&e.request.method==='GET'){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));}return res;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
+self.addEventListener('message',e=>{if(e.data==='skipWaiting')self.skipWaiting();});
